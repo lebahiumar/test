5fb521fd954487d4c2b039e923c4f41353439d73
lebahiumar <contact@iamumar.com>
2025-06-06 02:37:45 -0700

    name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.a4f.scira',
-  appName: 'scira-app',
+  appName: 'A4F',
   webDir: 'out'
 };
 
